0594c8b75c3696d1b2b8776d4b7c42b6280575ee
bobchucas <bobchucas@gmail.com>
2013-07-23 12:23:48 +0100

    Modified to access individual product pages

--- src/com/fashionintelligence/newlook/Newlook.java
+++ src/com/fashionintelligence/newlook/Newlook.java
@@ -1,11 +1,9 @@
 package com.fashionintelligence.newlook;
 
 import java.io.BufferedReader;
-import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 
 public class Newlook {
@@ -13,16 +11,15 @@ public class Newlook {
 	public static void main(String[] args) {
 		ArrayList<String> siteList = new ArrayList<String>();
 		try {
-			URL url = new URL("http://www.newlook.com/sitemap.xml");
-			ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-			FileOutputStream fos = new FileOutputStream("information.xml");
-			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-			fos.close();
-			BufferedReader rd = new BufferedReader(new FileReader(
-					"information.xml"));
+			// URL url = new URL("http://www.newlook.com/sitemap.xml");
+			// ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+			// FileOutputStream fos = new FileOutputStream("information.xml");
+			// fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+			// fos.close();
+			BufferedReader rd = new BufferedReader(
+					new FileReader("sitemap.xml"));
 			String pageLine;
 			while ((pageLine = rd.readLine()) != null) {
-				System.out.println(pageLine);
 				if (pageLine.contains("<loc>")
 						&& pageLine.contains("newlook.com/shop/")
 						&& pageLine.length() > 18)
@@ -34,11 +31,27 @@ public class Newlook {
 
 		} finally {
 
-			if(siteList.size()>0){
-			for (int i = 0; i < siteList.size(); i++) {
-				System.out.println(siteList.get(i));
-			}
-			}else{
+			if (siteList.size() > 0) {
+				for (int i = 0; i < siteList.size(); i++) {
+					URL productUrl;
+					BufferedReader in;
+					try {
+						// System.out.println(siteList.get(i));
+						productUrl = new URL(siteList.get(i).toString());
+						in = new BufferedReader(new InputStreamReader(
+								productUrl.openStream()));
+						String productLine = in.readLine();
+						// while ((productLine=in.readLine())!=null){
+						System.out
+								.println(siteList.get(i) + ", " + productLine);
+						// }
+					} catch (Exception e) {
+
+					} finally {
+
+					}
+				}
+			} else {
 				System.out.println("Array empty");
 			}
 		}

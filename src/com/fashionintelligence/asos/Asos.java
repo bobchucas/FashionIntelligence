c9556da6e890b614e5888338f9eb3a070902cbeb
Bobby Chucas <bchucas@deloitte.co.uk>
2013-07-30 12:02:03 +0100

    Colours, price, sale sorted. Size not incorporated for skus as loads following selection of colour

--- src/com/fashionintelligence/asos/Asos.java
+++ src/com/fashionintelligence/asos/Asos.java
@@ -20,6 +20,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Asos {
 
@@ -43,20 +44,22 @@ public class Asos {
 					BufferedWriter bw = new BufferedWriter(fw);
 					bw.write("ID,Name,Size Choice,Colour Choice,Availability,Price,Category1,Category2,Sale,Image,Date,URL");
 					bw.newLine();
-					WebDriver driver = new FirefoxDriver();
+					WebDriver driver = new HtmlUnitDriver();
 					DateFormat dateFormat = new SimpleDateFormat(
 							"yyyy/MM/dd HH:mm:ss");
-					for (int i = 0; i < 10; i++) {
+					for (int i = 300; i < 400; i++) {
+						String address= pageList.get(i);
+						if(address.contains("Prod/pgeproduct")){
 						String[] propertyString = null;
 						try {
 							try {
-								driver.get(pageList.get(i));
+								driver.get(address);
 							} catch (Exception e) {
-								driver = new FirefoxDriver();
-								driver.get(pageList.get(i));
+								driver = new HtmlUnitDriver();
+								driver.get(address);
 							}
-							System.out.println(i);
-							// propertyString = extractProperties(driver);
+							System.out.println("Accessing product "+i+", "+address);
+							 propertyString = extractProperties(driver);
 							if (propertyString != null) {
 								int colours = Integer
 										.parseInt(propertyString[3]);
@@ -118,8 +121,11 @@ public class Asos {
 								driver.quit();
 							} catch (Exception e) {
 							}
-							driver = new FirefoxDriver();
+							driver = new HtmlUnitDriver();
 						}
+						}else{
+							System.out.println("Non-product URL");
+					}
 					}
 					System.out.println("Loop finished");
 					bw.close();
@@ -136,9 +142,6 @@ public class Asos {
 	}
 
 	public static String[] extractProperties(WebDriver driver) {
-		if (!productPageCheck(driver)) {
-			return null;
-		} else {
 			String colour = extractColour(driver);
 			String[] breadcrumb = extractBreadcrumb(driver);
 			String[] string = { extractId(driver), extractName(driver),
@@ -146,36 +149,25 @@ public class Asos {
 					extractPrice(driver), breadcrumb[0], breadcrumb[1],
 					sale(driver), extractImage(driver) };
 			return string;
-		}
-	}
-
-	public static boolean productPageCheck(WebDriver driver) {
-		List<WebElement> productPage = driver.findElements(By
-				.className("product_display"));
-		if (productPage.size() > 0) {
-			return true;
-		} else {
-			return false;
-		}
 	}
 
 	public static String extractId(WebDriver driver) {
-		return driver.findElement(By.cssSelector("span[itemprop=productID]"))
+		return driver.findElement(By.cssSelector("span.productcode"))
 				.getText();
 	}
 
 	public static String extractName(WebDriver driver) {
-		return driver.findElement(By.cssSelector("h1[itemprop=name]"))
+		return driver.findElement(By.cssSelector("span.product_title"))
 				.getText();
 	}
 
 	public static String extractSize(WebDriver driver) {
 		try {
 			List<WebElement> sizeDropdown = driver.findElements(By
-					.id("size_standard"));
-			if (sizeDropdown.size() > 0 && sizeDropdown.get(0).isDisplayed()) {
+					.className("size"));
+			if (sizeDropdown.size() > 0) {
 				List<WebElement> sizes = sizeDropdown.get(0).findElements(
-						By.cssSelector("option[value]"));
+						By.cssSelector("div select option[value]"));
 				return Integer.toString(sizes.size());
 			} else {
 				return "1";
@@ -186,25 +178,19 @@ public class Asos {
 	}
 
 	public static String extractColour(WebDriver driver) {
-		try {
-			waitForElement(driver.findElement(By.className("colour")));
-			WebElement colourOptions = driver.findElements(
-					By.className("colour")).get(0);
-			if (colourOptions != null) {
-				return Integer.toString(colourOptions.findElements(
-						By.className("colour-option")).size());
+			List<WebElement> colourOptions = driver.findElements(
+					By.className("colour"));
+			if (colourOptions.size()>0) {
+				return Integer.toString(colourOptions.get(0).findElements(
+						By.cssSelector("option")).size()-1);
 			} else {
 				return "1";
 			}
-		} catch (Exception e) {
-			return "1";
-		}
 	}
 
 	public static String extractAvailability(WebDriver driver) {
-		WebElement stock = driver.findElements(By.id("out_stock_message")).get(
-				0);
-		if (stock.isDisplayed()) {
+		List<WebElement> stock = driver.findElements(By.className("outofstock"));
+		if (stock.size()>0 && stock.get(0).isDisplayed()) {
 			return "Out of stock";
 		} else {
 			return "In stock";
@@ -212,16 +198,15 @@ public class Asos {
 	}
 
 	public static String extractPrice(WebDriver driver) {
-		return driver.findElement(By.cssSelector("span[itemprop=price]"))
+		return driver.findElement(By.cssSelector("div.product_price span.product_price_details"))
 				.getText();
 	}
 
 	public static String sale(WebDriver driver) {
 		try {
-			WebElement colourOptions = driver.findElement(By.className("was"));
+			WebElement colourOptions = driver.findElement(By.cssSelector("div.product_price span.previousprice"));
 			if (colourOptions != null) {
-				return colourOptions.findElement(By.className("promovalue"))
-						.getText();
+				return colourOptions.getText().split(" ")[1].trim();
 			} else {
 				return "No sale";
 			}
@@ -231,21 +216,25 @@ public class Asos {
 	}
 
 	public static String[] extractBreadcrumb(WebDriver driver) {
-		WebElement breadcrumb = driver.findElement(By.className("breadcrumb"));
-		List<WebElement> crumbs = breadcrumb.findElements(By.cssSelector("li"));
-		String mainCat = crumbs.get(1).getText().replace(",", "");
-		String subCat = crumbs.get(2).getText().replace(",", "");
+		WebElement breadcrumb = driver.findElement(By.cssSelector("div.breadcrumbs span"));
+		List<WebElement> crumbs = breadcrumb.findElements(By.cssSelector("a"));
+		if(crumbs.size()>0){
+		String mainCat = crumbs.get(0).getText().replace(",", "");
+		String subCat = crumbs.get(1).getText().replace(",", "");
 		String[] crumb = { mainCat, subCat };
 		return crumb;
+		}else{
+			String[] crumb = {"-","-"};
+			return crumb;
+		}
 	}
 
 	public static String[] getColours(WebDriver driver) {
 		List<WebElement> colourOptions = driver.findElements(By
-				.className("colour-option"));
+				.cssSelector("div.colour select option"));
 		String[] colourList = new String[colourOptions.size()];
-		for (int i = 0; i < colourOptions.size(); i++) {
-			colourList[i] = colourOptions.get(i)
-					.findElement(By.cssSelector("a")).getAttribute("title");
+		for (int i = 0; i < colourOptions.size()-1; i++) {
+			colourList[i] = colourOptions.get(i+1).getAttribute("value");
 		}
 		return colourList;
 
@@ -264,22 +253,9 @@ public class Asos {
 	}
 
 	public static String extractImage(WebDriver driver) {
-		String imageUrl = driver.findElement(By.cssSelector("li.li_thumb img"))
+		String imageUrl = driver.findElement(By.cssSelector("img.main-image"))
 				.getAttribute("src");
-		return imageUrl.substring(0, imageUrl.indexOf("?"));
-	}
-
-	public static void waitForElement(WebElement element) {
-		for (int second = 0; second < 5; second++) {
-			if (element.isDisplayed()) {
-				break;
-			}
-			try {
-				Thread.sleep(1000);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+		return imageUrl;
 	}
 
 	public static ArrayList<String> extractBaseSitemap(URL url) {
@@ -300,8 +276,8 @@ public class Asos {
 				Matcher m = p.matcher(pageLine);
 				while (m.find()) {
 					String address = m.group().split("</loc>")[0];
-					if (isValidUrl(address)) {
-						pageList.add(m.group().split("</loc>")[0]);
+					if (isValidProductUrl(address)) {
+						pageList.add(address);
 					}
 				}
 			}
@@ -318,7 +294,7 @@ public class Asos {
 			try {
 				URL url = new URL(siteList.get(i));
 				pageList.addAll(extractBaseSitemap(url));
-				System.out.println(i);
+				System.out.println("Parsing sitemap "+i);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -327,7 +303,7 @@ public class Asos {
 		return pageList;
 	}
 
-	public static boolean isValidUrl(String string) {
+	public static boolean isValidProductUrl(String string) {
 		if (string.startsWith("http") && string.contains("asos.com")) {
 			return true;
 		}else{

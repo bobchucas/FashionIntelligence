907837bbc2449474b056ed44d0b0dacb5c9f455a
Bobby Chucas <bchucas@deloitte.co.uk>
2013-08-12 16:01:18 +0100

    Zara config

--- src/test/java/edu/uci/ics/crawler4j/zara/BasicCrawler.java
+++ src/test/java/edu/uci/ics/crawler4j/zara/BasicCrawler.java
@@ -18,6 +18,9 @@
 package edu.uci.ics.crawler4j.zara;
 
 import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -27,6 +30,7 @@ import org.openqa.selenium.WebDriver;
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.topshop.Topshop;
 import edu.uci.ics.crawler4j.url.WebURL;
 
 /**
@@ -34,6 +38,8 @@ import edu.uci.ics.crawler4j.url.WebURL;
  */
 public class BasicCrawler extends WebCrawler {
 	private int i=0;
+	private int j=0;
+	private String productString = "id=\"product\"";
 
 	private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g" + "|png|tiff?|mid|mp2|mp3|mp4"
 			+ "|wav|avi|mov|mpeg|ram|m4v|pdf" + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
@@ -54,35 +60,71 @@ public class BasicCrawler extends WebCrawler {
 	 */
 	@Override
 	public void visit(Page page, PrintWriter bw, WebDriver driver) {
-		int docid = page.getWebURL().getDocid();
+		DateFormat dateFormat = new SimpleDateFormat(
+				"yyyy/MM/dd HH:mm:ss");
 		String url = page.getWebURL().getURL();
-		String domain = page.getWebURL().getDomain();
-		String path = page.getWebURL().getPath();
-		String subDomain = page.getWebURL().getSubDomain();
-		String parentUrl = page.getWebURL().getParentUrl();
-		String anchor = page.getWebURL().getAnchor();
-
-//		System.out.println("Docid: " + docid);
-		System.out.println("URL: " + url);
-//		System.out.println("Domain: '" + domain + "'");
-//		System.out.println("Sub-domain: '" + subDomain + "'");
-//		System.out.println("Path: '" + path + "'");
-//		System.out.println("Parent page: " + parentUrl);
-//		System.out.println("Anchor text: " + anchor);
+		System.out.println(i+": "+url);
 		
 		if (page.getParseData() instanceof HtmlParseData) {
 			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-			String text = htmlParseData.getText();
 			String html = htmlParseData.getHtml();
-			List<WebURL> links = htmlParseData.getOutgoingUrls();
+			if(html.contains(productString)){
+//			List<WebURL> links = htmlParseData.getOutgoingUrls();
+//			System.out.println("Product #: " + j+" from "+i);
+			String[] propertyString = null;
+			try {
+				driver.get(url);
+				System.out.println(url);
+				propertyString = Topshop.extractProperties(driver);
+				if(propertyString!=null){
+				if (propertyString[4] != null) 
+					{
+					String[] sizeString = null;
+					sizeString = Topshop.getSizes(driver);
+					String [] stockString = null;
+					stockString = Topshop.getStock(driver);
+					int sizes = sizeString.length;
+					if(!propertyString[8].contains("sale")){
+						String sale=propertyString[2];
+						propertyString[2]=propertyString[8];
+						propertyString[8]=sale;
+					}
+					for(int k=0; k<sizes; k++)
+					{
+						if(sizes>1)
+						{
+						propertyString[4] = sizeString[k];
+						propertyString[5] = stockString[k];
+						}
+						else
+						{
+							propertyString[4] = "-";
+							propertyString[5] = "-";
+						}
+						for (int j = 0; j < propertyString.length; j++) 
+						{
+							bw.append(propertyString[j]
+											+ ",");
+						}	
+						Date date = new Date();
+						bw.append(dateFormat.format(date) + ",");
+						bw.append(url);
+						bw.append("\r\n");
+						}
+					}
+				}
+			}
+			catch (Exception e) {
+				e.printStackTrace();
 
-//			System.out.println("Text length: " + text.length());
-//			System.out.println("Html length: " + html.length());
-			System.out.println("Number of outgoing links: " + links.size());
-			System.out.println("i: " + i);
+			} finally {
+
+			}
+			j++;
+			}
 		}
 
-		Header[] responseHeaders = page.getFetchResponseHeaders();
+//		Header[] responseHeaders = page.getFetchResponseHeaders();
 //		if (responseHeaders != null) {
 //			System.out.println("Response headers:");
 //			for (Header header : responseHeaders) {
@@ -90,7 +132,6 @@ public class BasicCrawler extends WebCrawler {
 //			}
 //		}
 		
-		System.out.println("=============");
 		i++;
-	}
+}
 }
---
+++ src/test/java/edu/uci/ics/crawler4j/zara/Zara.java
@@ -0,0 +1,127 @@
+package edu.uci.ics.crawler4j.zara;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Zara {
+
+	public static void main(String[] args) {
+	}
+
+	public static String[] extractProperties(WebDriver driver) {
+		String[] breadcrumb = extractBreadcrumb(driver);
+		String[] string = { extractId(driver), extractName(driver),
+				extractPrice(driver), extractColour(driver),
+				extractSize(driver), extractAvailability(driver),
+				breadcrumb[0], breadcrumb[1], sale(driver) };
+		return string;
+	}
+
+	public static String extractId(WebDriver driver) {
+		return driver.findElement(By.className("reference")).getText().trim()
+				.split(" ")[1];
+	}
+
+	public static String extractName(WebDriver driver) {
+		return driver.findElement(By.cssSelector("div.right header h1"))
+				.getText().trim();
+	}
+
+	public static String extractSize(WebDriver driver) {
+		List<WebElement> multipleValues = driver.findElement(
+				By.className("size-select")).findElements(
+				By.className("product-size"));
+		if (multipleValues != null) {
+			return Integer.toString(multipleValues.size());
+		} else
+			return "1";
+	}
+
+	public static String extractAvailability(WebDriver driver) {
+		return "In Stock";
+	}
+
+	public static String extractColour(WebDriver driver) {
+		List<WebElement> colourBox = driver.findElement(By.className("colors")).findElements(By.cssSelector("label"));
+		if (colourBox != null) {
+			return Integer.toString(colourBox.size());
+		}
+		else{
+		return "1";
+	}
+	}
+
+	public static String extractPrice(WebDriver driver) {
+		String price = driver.findElement(
+				By.className("price")).getText().trim().split(" ")[0];
+		return "£"+price;
+	}
+
+	public static String sale(WebDriver driver) {
+		try {
+			String salePrice = driver.findElement(
+					By.cssSelector("p.price span.diagonal-line")).getText().trim().split(" ")[0];
+			if (salePrice != null) {
+				return "£"+salePrice;
+			} else {
+				return "No sale";
+			}
+		} catch (Exception e) {
+			return "No sale";
+		}
+	}
+
+	public static String[] extractBreadcrumb(WebDriver driver) {
+		WebElement breadcrumb = driver.findElement(By.cssSelector("div.breadcrumb"));
+		List<WebElement> crumbs = breadcrumb.findElements(By.cssSelector("li"));
+		String mainCat = "-";
+		String subCat = "-";
+		switch (crumbs.size()) {
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			mainCat = crumbs.get(1).getText();
+		case 4:
+			mainCat = crumbs.get(1).getText();
+			subCat = crumbs.get(2).getText();
+		default:
+			mainCat = crumbs.get(1).getText();
+			subCat = crumbs.get(2).getText();
+			for (int i = 0; i < crumbs.size() - 4; i++) {
+				subCat += ";" + crumbs.get(i + 3);
+			}
+			break;
+		}
+		String[] crumb = { mainCat, subCat };
+		return crumb;
+	}
+
+	public static String[] getSizes(WebDriver driver) {
+		List<WebElement> multipleValues = driver.findElement(
+				By.className("size-select")).findElements(
+				By.className("product-size"));
+		
+		String[] sizeList = {"do", "this"};
+		return sizeList;
+	}
+
+	public static void waitForElement(WebElement element) {
+		for (int second = 0; second < 5; second++) {
+			if (element.isDisplayed()) {
+				break;
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
